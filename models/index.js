3e9683bad3626d1fb9fd363245cacc80f1fa6168
jshvaron <jshva@icloud.com>
2023-04-17 16:12:06 -0500

    made some syntax adjustments to the relationships

--- models/index.js
+++ models/index.js
@@ -7,15 +7,21 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.hasOne(Category, {
   foreignKey: 'Category_id',
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-})
+});
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag,{through:'ProductTag'});
+Product.belongToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {through:'ProductTag'});
+Tag.belongToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 
 module.exports = {
