844f5577189b272a82276208a0393c92b0f515ac
jshvaron <jshva@icloud.com>
2023-04-18 15:53:29 -0500

    Route: Delete tag complete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -53,8 +53,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const deleteTag = await Tag.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(deleteTag)
+  } catch(err){
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
