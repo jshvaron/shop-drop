356e6a8be1f3bfc3baba0ca21120ea21f9b8ce10
jshvaron <jshva@icloud.com>
2023-04-18 15:36:21 -0500

    Route: get route for one product w/ category and tag data completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,16 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id` w/associated Category and Tag data
+  try{
+    const productById = await Product.findPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(productById);
+  } catch(err){
+    res.status(400).json(err);
+  }
 });
 
 // create new product
