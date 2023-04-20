5f14f0fcdbcf35b04492d4b05d4e1fd459489aeb
jshvaron <jshva@icloud.com>
2023-04-20 00:01:31 -0500

    npm issues resolved

--- server.js
+++ server.js
@@ -13,4 +13,28 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+
+
+
+  
 });
+
+
+// NPM trouble shoot notes
+// reinstalled npm X - installs packs but does nothing else
+// npm install --package-lock-only X - hangs
+// recloned repo X
+// inflate:node? X - hangs
+// imported json packsfrom other repo X
+// npm cache clean  --force X
+// tried npm i and other packages in different repos, same issues/errors. I think this is a device issue.
+// npm install -g npm@latest X - literally does nothing in (repo dir and main dir)
+// npm install -g npm X - literally does nothing (repo dir and main dir)
+// node -v v18.13.0
+// npm -v 9.6.3
+// brew install node => npm install -g npm X same issues presented itself from line33
+// hail mary restarted apps and device. No luck
+// npm update or npm update --save-dev (term:up to date in 38ms)(integrated vs term: hangs: (⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂⠂) ⠦ idealTree:shop-drop: sill)
+// npm outdated X - Does nothing
+// npm init -y again = progress, created new pk.json
+// npm install again = new error: npm ERR! code ENOTEMPTY
